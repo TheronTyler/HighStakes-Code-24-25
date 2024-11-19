fd2cfbb536673a03be7271d6d47680292871786a
Theron Tyler <114770047+TheronTyler@users.noreply.github.com>
2024-11-19 12:40:31 -0800

    Added static prefix to devices, resolving build error. The auton selector worked!

--- Auton-Selector/include/robot-config.h
+++ Auton-Selector/include/robot-config.h
@@ -2,26 +2,26 @@
 #include "vex.h"
 using namespace vex;
 
-competition Competition;
+static competition Competition;
 
-brain Brain;
-controller Controller;
+static brain Brain;
+static controller Controller;
 
-motor fLDrive = motor(PORT13, true);
-motor bLDrive = motor(PORT11, true);
-motor mLDrive = motor(PORT12, true);
-motor fRDrive = motor(PORT1);
-motor bRDrive = motor(PORT2);
-motor mRDrive = motor(PORT3);
+static motor fLDrive = motor(PORT13, true);
+static motor bLDrive = motor(PORT11, true);
+static motor mLDrive = motor(PORT12, true);
+static motor fRDrive = motor(PORT1);
+static motor bRDrive = motor(PORT2);
+static motor mRDrive = motor(PORT3);
 
-motor intake = motor(PORT14);
+static motor intake = motor(PORT14);
 
-motor arm = motor(PORT9);
+static motor arm = motor(PORT9);
 
-inertial sense = inertial(PORT5);
-rotation armAngle = rotation(PORT20);
-aivision ai = aivision(PORT10);
+static inertial sense = inertial(PORT5);
+static rotation armAngle = rotation(PORT20);
+static aivision ai = aivision(PORT10);
 
-digital_out moGo = digital_out(Brain.ThreeWirePort.A);
-digital_out doinker = digital_out(Brain.ThreeWirePort.H);
-digital_out wallStake = digital_out(Brain.ThreeWirePort.G);
+static digital_out moGo = digital_out(Brain.ThreeWirePort.A);
+static digital_out doinker = digital_out(Brain.ThreeWirePort.H);
+static digital_out wallStake = digital_out(Brain.ThreeWirePort.G);
